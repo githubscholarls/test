94bbd473b17d19027257a8f463ecda5b3e2ae030
lishuai_schoalr <2363502880@qq.com>
2023-07-03 16:31:55 +0800

    添加linux下dump准备

---
+++ WebApiTest/Controllers/DEBUGController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Permissions;
+
+namespace WebApiTest.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DeBugController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestCrashDump()
+        {
+            //1. crash
+            Task.Factory.StartNew(() =>
+            {
+                Test("a");
+            });
+            return Ok();
+        }
+        [NonAction]
+        public static string Test(string a)
+        {
+            return Test("a" + a.Length);
+        }
+        [HttpGet]
+        public IActionResult TestCpuDump()
+        {
+            Task.Factory.StartNew(() => { bool b = true; while (true) { b = !b; } });
+            Task.Factory.StartNew(() => { bool b = true; while (true) { b = !b; } });
+            return Ok();
+        }
+    }
+}
