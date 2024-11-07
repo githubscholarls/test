c26c98306d41f8fe30b382c631a677902582d78e
帅 李 <2363502880@qq.com>
2024-11-07 14:14:03 +0800

    vue-element 登录和首页

--- WebApiTest/Controllers/VueAdminTemplate.cs
+++ WebApiTest/Controllers/VueAdminTemplate.cs
@@ -16,7 +16,13 @@ namespace WebApiTest.Controllers
         public IActionResult login()
         {
             //return Ok(Newtonsoft.Json.JsonConvert.SerializeObject(new { username = "lishuai", code = 20000 }));
-            return new JsonResult(new { username = "lishuai", code = 20000 });
+            return new JsonResult(new {  data=new { name = "lishuai", token = "this is token" }, code = 20000  });
+        }
+        [HttpGet]
+        public IActionResult info()
+        {
+            //return Ok(Newtonsoft.Json.JsonConvert.SerializeObject(new { username = "lishuai", code = 20000 }));
+            return new JsonResult(new { data = new { name = "lishuai", token = "this is token" }, code = 20000 });
         }
     }
 
