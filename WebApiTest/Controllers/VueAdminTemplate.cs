c0d7079aec70951df914105f457681f13b9fda95
帅 李 <2363502880@qq.com>
2024-11-12 10:01:14 +0800

    .

--- WebApiTest/Controllers/VueAdminTemplate.cs
+++ WebApiTest/Controllers/VueAdminTemplate.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiTest.Controllers
 {
+
+    public class ApiResponse<T>
+    {
+        public T data { get; set; }
+        public int code { get; set; } = 20000;
+    }
     [Route("vue-admin-template/user/[action]")]
-    public class VueAdminTemplate : ControllerBase
+    public class VueAdminUserTemplate : ControllerBase
     {
         [HttpGet]
         public IActionResult Index()
@@ -24,6 +30,30 @@ namespace WebApiTest.Controllers
             //return Ok(Newtonsoft.Json.JsonConvert.SerializeObject(new { username = "lishuai", code = 20000 }));
             return new JsonResult(new { data = new { name = "lishuai", token = "this is token" }, code = 20000 });
         }
+    }
+
+
+    [Route("vue-admin-template/table/[action]")]
+    public class VueAdminTableTemplate : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult list()
+        {
+            var items = new List<string>() { "撒发射点", "sf法撒旦", "的萨芬" };
+            var res = new ListRes() { data = new ListData() { items = items } };
+            return new JsonResult(new ApiResponse<ListRes>() { data = res });
+
+        }
+
+        public class ListRes
+        {
+            public ListData data { get; set; } = new();
+        }
+        public class ListData
+        {
+            public List<string> items { get; set; } = new();
+        }
+
     }
 
     
