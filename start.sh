dc12043efe6e97908abf73d7fd8de04ac6d15008
帅 李 <2363502880@qq.com>
2024-11-28 15:46:08 +0800

    设置挂载文件到服务器

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ docker stop webapitest
 docker rm webapitest
 git pull
 docker build -t dockerfilewebapitest -f DockerfileWebApiTest  .
-docker run -itd --net mytestnet --ip 172.18.0.3 -p 18030:8080 --name  webapitest dockerfilewebapitest
+docker run -itd -v /root/volumes/webapitest/upload:/app/upload -v /root/volumes/webapitest/tmp:/app/tmp --net mytestnet --ip 172.18.0.3 -p 18030:8080 --name  webapitest dockerfilewebapitest
